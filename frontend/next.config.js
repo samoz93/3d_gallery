fd6ef89739d73a20f90c5c0aae2f4a655ab919ec
Samoz <samozayncom@gmail.com>
2024-01-01 23:17:35 +0300

    Add output option to next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   webpack: (config, { isServer }) => {
     config.module.rules.push({
       test: /\.glsl/,
