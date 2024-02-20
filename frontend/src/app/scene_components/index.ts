fc70275c16f36150aa8fa3406f9f6893e8558d83
Samoz <samozayncom@gmail.com>
2024-02-20 22:04:37 +0300

    Add Electric scene component

--- frontend/src/app/scene_components/index.ts
+++ frontend/src/app/scene_components/index.ts
@@ -3,9 +3,9 @@ export * from "./AudioVisScene";
 export * from "./AudioVis_2Scene";
 export * from "./BrainScene";
 export * from "./DancerScene";
+export * from "./Electric";
 export * from "./FancyScroll";
 export * from "./PlayfulChildren";
 export * from "./PlumbusSphereScene";
 export * from "./RayMarchingScene";
 export * from "./ScorchingSun";
-export 
